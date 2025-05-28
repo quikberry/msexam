f837bc09cf9cb49bcf844f2825e4828f63bb50ff
Sergey Movchan <quikberry@list.ru>
2025-05-28 13:29:45 +0300

    adminka

--- exam/admin.py
+++ exam/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import smexam
 
-# Register your models here.
+@admin.register(smexam)
+class SMExamAdmin(admin.ModelAdmin):
+    list_display = ("title", "exam_date", "created_at", "is_public")
+    list_filter = ("is_public", "created_at", "exam_date")
+    search_fields = ("title", "students__email")
+    filter_horizontal = ("students",)
